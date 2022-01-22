07089e80775845ff75ce40c928fb7ee6a4ae6cdd
Supun222 <74459467+Supun222@users.noreply.github.com>
2022-01-22 10:46:32 +0530

    Sign up repository made

--- repositories/userRepository.js
+++ repositories/userRepository.js
@@ -1,3 +1,4 @@
+const { reject } = require("bcrypt/promises");
 const bcrpt = require("bycrpt");
 
 let _db;
@@ -27,6 +28,51 @@ function getUserByEmailAndPassword(email, password){
 }
 
 function signUpUser(name,email,passwordOne, passwordTwo){
-    const sql2 = `SELECT id, name, email, password FROM users WHERE email= ?`; 
-
+    const sql1 = `SELECT id, name, email, password FROM users WHERE email= ?`; 
+    
+    return new Promise(async (resolve, reject) => {
+        const data = {};
+        if(
+            name.length < 1 ||
+            email.length < 1 ||
+            passwordOne.length < 1 ||
+            passwordTwo.length < 1 
+        ) {
+            data.error = "vlues missing";
+            reject(data);
+        } else {
+            knex_db.raw(sql1, [email])
+                .then(async(user) => {
+                    if(!(user[0] == undefined)) {
+                        data.error = "Already Registered";
+                        reject(data);
+                    
+                    } else {
+                        if(passwordOne != passwordTwo) {
+                            data.error = "Passwords Mismatch";
+                            reject(data);
+                        } else {
+                            const hashPassword = await bcrypt.hash(passwordTwo, 10);
+                            const sql = `INSERT INTO users(id,name,email,password) VALUES(NULL,?,?,?)`;
+                            knex_db.raw(sql, [name,email.hashPassword])
+                            .then(() => {
+                                resolve();
+                            })
+                            .catch((error) =>{
+                                reject(error)
+                            })
+                        }
+                    }
+                })
+                .catch((error) => {
+                    reject(eror)
+                })
+            
+        }
+    })
 }
+
+module.exports = {
+    getUserByEmailAndPassword,
+    signUpUser,
+};
